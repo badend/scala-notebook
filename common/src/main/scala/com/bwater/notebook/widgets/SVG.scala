0e6ab244f5fc3ac3c7c043dd83d616b639726bd7
Nathan Hamblen <nathan@technically.us>
2013-04-11 21:45:02 -0400

    svg base trait

---
+++ common/src/main/scala/com/bwater/notebook/widgets/SVG.scala
@@ -0,0 +1,16 @@
+package com.bwater.notebook.widgets
+
+import com.bwater.notebook._
+
+trait SVG {
+  def data: Seq[(Double, Double)]
+  val (d1, d2) = data.unzip
+
+  val (min1, min2) = (d1.min, d2.min)
+  val (max1, max2) = (d1.max, d2.max)
+  val sz = math.max(max1 - min1, max2 - min2)
+  val viewbox = Seq(min1, -1*max2, max1 - min1, max2 - min2).mkString(" ")
+
+  val width = "400px"
+  val height = "200px"
+}
